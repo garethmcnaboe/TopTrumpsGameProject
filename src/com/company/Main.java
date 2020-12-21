2806ac6b581ce82c285826ba33681e13f0786766
Gareth McNaboe <gareth.mcnaboe.2021@mumail.com>
2020-12-21 15:21:51 +0000

    Commit 4 - Dealing into LinkedList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //create an array called card of 10 card objects.
+        //calling method to show home screen
+        HomeScreen.printHomeScreen();
+
+        //create an array called cardArray of 10 card objects.
         Card[] cardArray = new Card[10];
 
-        //Calling method which will create objects which will contain the player card data.
+        //calling method which will create objects which will contain the player card data.
         CardData.createCards(cardArray);
 
-        //Calling method to shuffle the deck of cards
+        //calling method to shuffle the deck of cards
         Deck.shuffleArray(cardArray);
-
-
     }
 }
