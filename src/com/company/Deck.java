4e6dff5cf1e195aedf1eb0e766482e5fe4629409
Gareth McNaboe <gareth.mcnaboe.2021@mumail.com>
2020-12-21 15:21:51 +0000

    Commit 3

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -16,9 +16,6 @@ public class Deck {
             cardArray[index] = cardArray[i];
             cardArray[i] = a;
         }
+        System.out.println("Shuffle of deck complete.");
     }
-
-
-
-
 }
